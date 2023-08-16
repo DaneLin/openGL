14658236caa942ddb002b35822fed25280c77ca3
DaneLin <825658797@qq.com>
2023-08-16 09:16:17 +0800

    完成摄像机联系

--- include/camera.h
+++ include/camera.h
@@ -87,33 +87,26 @@ public:
     // Custom implementation of the LookAt function
     glm::mat4 calculate_lookAt_matrix(glm::vec3 position, glm::vec3 target, glm::vec3 worldUp)
     {
-        // 1. Position = known
-        // 2. Calculate cameraDirection
+        // 计算方向法线，即z轴
         glm::vec3 zaxis = glm::normalize(position - target);
-        // 3. Get positive right axis vector
-        glm::vec3 xaxis = glm::normalize(glm::cross(glm::normalize(worldUp), zaxis));
-        // 4. Calculate camera up vector
+        // 计算x轴
+        glm::vec3 xaxis = glm::normalize(glm::cross(worldUp, zaxis));
+        // 计算y轴，通过叉乘得到
         glm::vec3 yaxis = glm::cross(zaxis, xaxis);
 
-        // Create translation and rotation matrix
-        // In glm we access elements as mat[col][row] due to column-major layout
-        glm::mat4 translation = glm::mat4(1.0f); // Identity matrix by default
-        translation[3][0] = -position.x;         // Third column, first row
-        translation[3][1] = -position.y;
-        translation[3][2] = -position.z;
-        glm::mat4 rotation = glm::mat4(1.0f);
-        rotation[0][0] = xaxis.x; // First column, first row
-        rotation[1][0] = xaxis.y;
-        rotation[2][0] = xaxis.z;
-        rotation[0][1] = yaxis.x; // First column, second row
-        rotation[1][1] = yaxis.y;
-        rotation[2][1] = yaxis.z;
-        rotation[0][2] = zaxis.x; // First column, third row
-        rotation[1][2] = zaxis.y;
-        rotation[2][2] = zaxis.z;
+        glm::mat4 trans;
+        glm::mat4 view;
+        //in glm we access elements as mat[col][row] due to column-major layout
+        //glm中是列向量
+        for (int i = 0; i < 3; i++)
+        {
+            trans[3][i] = -position[i];
+            view[i][0] = xaxis[i];
+            view[i][1] = yaxis[i];
+            view[i][2] = zaxis[i];
+        }
 
-        // Return lookAt matrix as combination of translation and rotation matrix
-        return rotation * translation; // Remember to read from right to left (first translation then rotation)
+        return view * trans;
     }
 
     // processes input received from any keyboard-like input system
