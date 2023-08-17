1409ac2fdbf8224bfb92bbdd8e439b6df87054dc
DaneLin <825658797@qq.com>
2023-08-17 11:32:03 +0800

    新增纹理图片

--- src/openGL.cpp
+++ src/openGL.cpp
@@ -43,6 +43,9 @@ void mouse_callback(GLFWwindow *window, double xpos, double ypos);
 // 监听鼠标滚轮事件
 void scroll_callback(GLFWwindow *window, double xoffset, double yoffset);
 
+// 读取纹理
+unsigned int loadTexture(const char *path);
+
 int main()
 {
     // 初始化GLFW
@@ -304,3 +307,44 @@ void scroll_callback(GLFWwindow *window, double xoffset, double yoffset)
 {
     camera.ProcessMouseScroll(static_cast<float>(yoffset));
 }
+
+/// @brief utility function for loading a 2D texture from file
+/// @param path 对应纹理文件的路劲
+/// @return 返回读取后的纹理ID
+unsigned int loadTexture(char const * path)
+{
+    unsigned int textureID;
+    glGenTextures(1, &textureID);
+
+    int width, height, nrComponents;
+    unsigned char *data = stbi_load(path, &width, &height, &nrComponents, 0);
+    
+    if (data)
+    {
+        GLenum format;
+        if (nrComponents == 1)
+            format = GL_RED;
+        else if (nrComponents == 3)
+            format = GL_RGB;
+        else if (nrComponents == 4)
+            format = GL_RGBA;
+
+        glBindTexture(GL_TEXTURE_2D, textureID);
+        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    }
+    else
+    {
+        std::cout << "Texture failed to load at path: " << path << std::endl;
+    }
+
+    stbi_image_free(data);
+    return textureID;
+}
+
+
--- src/shader.fs
+++ src/shader.fs
@@ -10,9 +10,10 @@ in vec3 Normal;
 in vec3 FragPos;
 in vec3 LightPos;
 
+in vec2 TexCoords;
+
 struct Material {
-    vec3 ambient;
-    vec3 diffuse;
+    sampler2D diffuse;
     vec3 specular;
     float shininess;
 };
@@ -31,13 +32,14 @@ uniform Material material;
 void main()
 {
     //ambient
-    vec3 ambient = light.ambient *  material.ambient;
+    // vec3 ambient = light.ambient *  material.ambient;
+    vec3 ambient = light.ambient * vec3(texture(material.diffuse, TexCoords));
 
     //diffuse
     vec3 norm = normalize(Normal);
     vec3 lightDir = normalize(light.position - FragPos);
     float diff = max(dot(norm, lightDir), 0.0f);
-    vec3 diffuse = light.diffuse *  (diff * material.diffuse);
+    vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse, TexCoords));
 
     //specular
     vec3 viewDir = normalize(viewPos - FragPos);
